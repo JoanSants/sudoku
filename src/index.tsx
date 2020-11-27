44d5159206d49d2b81f44d729c899512fdb573a9
Joan Santos <joansants07@gmail.com>
2020-11-27 14:57:50 -0300

    Initial setup

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { reportWebVitals } from './core'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Hello Word</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+reportWebVitals()
