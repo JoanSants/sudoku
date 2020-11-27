c03c794b5fad6bbd350859dfa2ead757a0a11517
Joan Santos <joansants07@gmail.com>
2020-11-27 18:03:04 -0300

    Add styled-components theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
 import { reportWebVitals } from './core'
-import { GlobalStyles } from './styles'
+import { GlobalStyles, theme } from './styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <div>Hello Word</div>
-    </>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    background: 'radial-gradient(#283c34CC, #283c34)',
+    black: '283c34',
+    blue: 'a0e9fd',
+    lightBlue: 'caf3fe',
+    white: '#ffffff',
+  },
+  transition: '0.3s',
+}
+
+export default theme
