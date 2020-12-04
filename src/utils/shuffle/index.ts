c65248153096235ebccaf121a7c5718471f44458
Joan Santos <joansants07@gmail.com>
2020-12-04 17:36:51 -0300

    Create shuffle util

---
+++ src/utils/shuffle/index.ts
@@ -0,0 +1,6 @@
+export default (array: any[]) => {
+  for (let i = 0; i < array.length; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
