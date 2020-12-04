8ffe36d159c25fbc3e3d53254ed2d37276f203f7
Joan Santos <joansants07@gmail.com>
2020-12-04 17:39:09 -0300

    Create fill grid util

---
+++ src/utils/fill-grid/index.test.ts
@@ -0,0 +1,51 @@
+import { GRID } from '../../typings'
+
+import fillGrid from './'
+
+describe('fillGrid', () => {
+  it('fills an empty grid', () => {
+    const grid: GRID = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+
+    fillGrid(grid)
+
+    for (let row in grid) {
+      for (let col in grid) {
+        expect(grid[row][col]).toBeGreaterThanOrEqual(1)
+        expect(grid[row][col]).toBeLessThanOrEqual(9)
+      }
+    }
+  })
+
+  it('fills a valid partially filled grid', () => {
+    const grid: GRID = [
+      [1, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 9, 0],
+      [0, 0, 0, 0, 0, 0, 8, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 3, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 4, 0],
+      [0, 0, 0, 5, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 6, 0, 7, 0, 0],
+    ]
+
+    fillGrid(grid)
+
+    for (let row in grid) {
+      for (let col in grid) {
+        expect(grid[row][col]).toBeGreaterThanOrEqual(1)
+        expect(grid[row][col]).toBeLessThanOrEqual(9)
+      }
+    }
+  })
+})
