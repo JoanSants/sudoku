b4c52059599fb001a7edc63ba69ad786d662c956
Joan Santos <joansants07@gmail.com>
2020-12-07 13:57:26 -0300

    Add key binding

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,18 +1,63 @@
 import React, { Children, FC, useCallback, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import useMouseTrap from 'react-hook-mousetrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { AnyAction, Dispatch } from 'redux'
 
-import { INDEX } from '../../typings'
-import { createGrid } from '../../reducers'
+import { BLOCK_COORDS, INDEX } from '../../typings'
+import { createGrid, IReducer, selectBlock } from '../../reducers'
 import Block from './block'
 import { Container, Row } from './styles'
 
+interface IState {
+  selectedBlock?: BLOCK_COORDS
+}
+
 const Grid: FC = () => {
+  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
+    selectedBlock,
+  }))
   const dispatch = useDispatch<Dispatch<AnyAction>>()
   const create = useCallback(() => dispatch(createGrid()), [dispatch])
   useEffect(() => {
     create()
   }, [create])
+
+  useMouseTrap('down', () => {
+    if (state.selectedBlock && state.selectedBlock[0] < 8)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] + 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  })
+  useMouseTrap('left', () => {
+    if (state.selectedBlock && state.selectedBlock[1] > 0)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] - 1) as INDEX,
+        ])
+      )
+  })
+  useMouseTrap('right', () => {
+    if (state.selectedBlock && state.selectedBlock[1] < 8)
+      dispatch(
+        selectBlock([
+          state.selectedBlock[0],
+          (state.selectedBlock[1] + 1) as INDEX,
+        ])
+      )
+  })
+  useMouseTrap('up', () => {
+    if (state.selectedBlock && state.selectedBlock[0] > 0)
+      dispatch(
+        selectBlock([
+          (state.selectedBlock[0] - 1) as INDEX,
+          state.selectedBlock[1],
+        ])
+      )
+  })
 
   return (
     <Container>
