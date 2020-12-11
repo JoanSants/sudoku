2891ea87bb9e385bb2ecc7f75ae80060c989c72a
Joan Santos <joansants07@gmail.com>
2020-12-10 21:06:25 -0300

    Fix eslint

--- src/components/numbers/index.tsx
+++ src/components/numbers/index.tsx
@@ -4,9 +4,7 @@ import { Container } from './styles'
 import Button from './button'
 import { NUMBERS } from '../../typings'
 
-interface IProps {}
-
-const Numbers: FC = ({}: IProps) => (
+const Numbers: FC = () => (
   <Container>
     {([1, 2, 3, 4, 5, 6, 7, 8, 9] as NUMBERS[]).map((value) => (
       <Button key={value} value={value} />
